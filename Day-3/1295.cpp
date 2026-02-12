d8d8750fe9091d14bab99e238eb8a7ab2fa5c795
Rishabh Paudel <24BCS10110@cuchd.in>
2026-02-12 23:32:33 +0530

    added code to day3 and its not done i am doin rn

--- Day-3/1295.cpp
+++ Day-3/1295.cpp
@@ -1,1 +1,28 @@
+#include <vector>
 using namespace std;
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        int evenCount = 0;
+        
+        for (int i = 0; i<nums.size(); i++) {
+
+            int varie = nums[i];
+            
+            // int tmp = 0;
+
+            // while (varie > 0) {
+                int di = nums[i]/10;
+                varie = di;
+                count++;
+            // }
+            if (count%2 == 0) {evenCount++;}
+            // if (count%2 != 0) {evenCount = 0;}
+        }
+        // if (count%2 != 0) {evenCount = 0;}
+                 
+        return evenCount;
+    }
+};
