270165d282ac54369b0ff42267d3cc53854240bf
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-10 20:54:59 +0530

    added day12 brute force

---
+++ 12Day/12-238Bruteforce.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> answer(nums.size());
+
+        answer[0] = 1;      // any number before index will be considered as 1
+        int suffix = 1;
+
+        for(int i = 1; i<nums.size(); i++) {
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+        for(int i = nums.size()-1; i>= 0; i--) {
+            answer[i] = answer[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+
+        return answer;
+    }
+};
