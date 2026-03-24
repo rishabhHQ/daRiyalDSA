8ed9db7e3d55da47e3ecfacce9e30c585707a29a
Rishabh Paudel <24BCS10110@cuchd.in>
2026-03-24 20:15:37 +0530

    added day 14 yayaya

---
+++ 14-724.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int lsum =0;
+        // int rsum =0;
+        int tsum =0;
+
+        for(int i=0; i<nums.size(); i++) {
+            tsum += nums[i];
+        }
+        for(int i =0; i<nums.size();i++) {
+            if(lsum == (tsum - nums[i])) {
+                return i;
+            }
+            else {                
+                tsum = tsum - nums[i];
+                lsum = lsum + nums[i]; 
+            }
+        }
+        return -1;
+    }
+};
+
+// [ Time taken: 26m 43s ], no hint taken
