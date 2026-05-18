4a35d22d9410ca3de4a1060f6ccec05712eb611d
Rishabh Paudel <24BCS10110@cuchd.in>
2026-05-18 23:58:50 +0530

    added dxa

---
+++ 000dxa/pattern3.cpp
@@ -0,0 +1,16 @@
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+#include <iosteam>
+using namespace std;
+
+int main () {
+for(int i=0; i<3; i++) {
+    for(int j=0; j<3; j++) {
+        cout << j;
+    }
+    cout << "\n"
+}
+return 0;
+}
