a7fc50a6bfdcb9049a0d958b51fab08765b04564
Rishabh Paudel <24BCS10110@cuchd.in>
2026-05-19 00:53:17 +0530

    added pattexrns

--- 000dxa/p4.cpp
+++ 000dxa/p4.cpp
@@ -1,7 +1,4 @@
-// 1 2 3 4
-// 1 2 3 4
-// 1 2 3 4
-// 1 2 3 4
+// 4321
 
 #include <iostream>
 using namespace std;
@@ -10,8 +7,8 @@ int main () {
     int n;
     cin >> n;
     
-for(int i=1; i<=n; i++) {
-    for(int j=1; j<=n; j++) {
+for(int i=n; i>0; i--) {
+    for(int j=n; j>0; j--) {
         cout << j ;
     }
     cout << "\n";
---
+++ 000dxa/p6.cpp
@@ -0,0 +1,24 @@
+// *,**,***,****
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    for(int i=0; i<4; i++) {
+        for(int j=0; j<=i; j++) {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+  
+  
+  
+    // for(int i=0; i<4; i++) {
+    //     for(int j=i; j<4; j++) {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+return 0;
+}
