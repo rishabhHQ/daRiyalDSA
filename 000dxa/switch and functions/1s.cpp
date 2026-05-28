85cad8d1e4425cd3d6c0426fac673c90c4cac52e
Rishabh Paudel <24BCS10110@cuchd.in>
2026-05-28 22:41:12 +0530

    added switch

---
+++ 000dxa/switch and functions/1s.cpp
@@ -0,0 +1,17 @@
+// amt = 1330
+// 100, 50, 20, 1 rupee notes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int amt;
+    cin >> amt;
+
+    int c;
+    cin >> c;
+
+    while(amt > 0) {
+        // some code...
+    }
+}
